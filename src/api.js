30331781d67da3f194ccb28d86c45b72d5b81b64
Laevahteinn <cade.haynes@gmail.com>
2026-03-28 20:33:57 +1100

    Changed to specify the method for GetCharacters (functionally the same as before)

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 ﻿const BASE_URL = "https://localhost:7299/api";
 
 export async function getCharacters() {
-    const response = await fetch(`${BASE_URL}/Character`);
+    const response = await fetch(`${BASE_URL}/Character`, {
+        method: 'GET'
+    });
     return response.json();
 }
