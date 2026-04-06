8ee9bcb9c38f6242bb16aead9b54e0012e74410b
Laevahteinn <cade.haynes@gmail.com>
2026-04-06 17:17:47 +1000

    Character and Home now have navigation buttons to move between pages (currently just the two).

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,13 +1,21 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 export default function Characters({ characters }) {
+    let nav = useNavigate();
+
     return (
         <div>
+
             <h2>Characters</h2>
+
+            <button onClick={() => nav("/")}>Home</button>
 
             {characters.map(c => (
                 <div key={c.id}>
                     {c.name}
                 </div>
             ))}
+
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 export default function Home() {
+    let nav = useNavigate();
+
     return (
         <div>
             <h2>Home</h2>
+
+            <p><Link to="/characters">Characters</Link></p>
+
+            <button onClick={() => nav("/characters")}>Characters</button>
         </div>
     )
 }
